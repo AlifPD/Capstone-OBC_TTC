950affbbbb619022b80d231b4742db3099e19eaa
Muhammad Alif Putra Dafi <muhammadalifputradafi@gmail.com>
2023-04-21 12:52:54 +0700

    Add I2C to TTC

---
+++ TTC/Arduino IDE/I2C/I2C/I2C.ino
@@ -0,0 +1,104 @@
+// Include Arduino Wire library for I2C
+#include <Wire.h>
+
+// Define Slave I2C Address
+#define SLAVE_ADDR 94
+
+// Define Slave answer size
+#define ANSWERSIZE 8
+
+uint8_t buffer[8] = {0};
+
+// Define string with response to Master
+String answer = "Hello";
+
+void setup() {
+  SystemClock_Config();
+
+  // Initialize I2C communications as Slave
+  Wire.begin(SLAVE_ADDR);
+  
+  // Function to run when data requested from master
+  Wire.onRequest(requestEvent); 
+  
+  // Function to run when data received from master
+  Wire.onReceive(receiveEvent);
+  
+  // Setup Serial Monitor 
+  Serial.begin(115200);
+  Serial.println("I2C Slave Demonstration");
+}
+
+void receiveEvent(int howMany) {
+// Print to Serial Monitor
+  Serial.print("Data Received from I2C Master ... [");
+  // Read while data received
+  while (0 < Wire.available()) {
+    for(int i=0; i<8; i++){
+      byte x = Wire.read();
+      Serial.print(x);
+      if(i != 7){
+        Serial.print(", ");
+      } else {
+        Serial.println("]");
+      }
+    }
+  }
+}
+
+void requestEvent() {
+
+  // Setup byte variable in the correct size
+  byte response[ANSWERSIZE];
+  
+  // Format answer as array
+  for (byte i=0;i<ANSWERSIZE;i++) {
+    response[i] = (byte)answer.charAt(i);
+  }
+  
+  // Send response back to Master
+  Wire.write(response,sizeof(response));
+  
+  // Print to Serial Monitor
+  Serial.println("Request event");
+}
+
+void loop() {
+
+  // Time delay in loop
+  delay(50);
+}
+
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI_DIV2;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
